4dd6bd36dcdfed5418e8b590e821d2a12bd67857
syber <devnull@localhost>
2019-06-02 01:59:05 +0300

    net::sockaddr: add specific typemap

--- src/xs/net/sockaddr.h
+++ src/xs/net/sockaddr.h
@@ -12,7 +12,7 @@ namespace xs { namespace net {
 namespace xs {
     template <class TYPE> struct Typemap<panda::net::SockAddr*, TYPE> : TypemapBase<panda::net::SockAddr*, TYPE> {
         static panda::net::SockAddr* in (pTHX_ SV* arg) { return xs::net::_in_sockaddr_ptr(aTHX_ arg); }
-        static Sv create (pTHX_ const panda::net::SockAddr* var, const Sv& = {}) {
+        static Sv out (pTHX_ const panda::net::SockAddr* var, const Sv& = {}) {
             if (!var) return Sv::undef;
             return xs::net::_create_sockaddr(aTHX_ *var);
         }
@@ -20,6 +20,8 @@ namespace xs {
 
     template <> struct Typemap<panda::net::SockAddr> : TypemapBase<panda::net::SockAddr> {
         static panda::net::SockAddr in (pTHX_ SV* arg) { return xs::net::_in_sockaddr(aTHX_ arg); }
-        static Sv create (pTHX_ const panda::net::SockAddr& var, const Sv& = {}) { return xs::net::_create_sockaddr(aTHX_ var); }
+        static Sv out (pTHX_ const panda::net::SockAddr& var, const Sv& = {}) { return xs::net::_create_sockaddr(aTHX_ var); }
     };
+
+    template <> struct Typemap<const panda::net::SockAddr&> : Typemap<panda::net::SockAddr> {};
 }
