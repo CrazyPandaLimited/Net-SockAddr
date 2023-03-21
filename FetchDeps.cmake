f66223ad358c33bb3e29ea140e789da663ad7dfb
g.smorkalov <g.smorkalov@crazypanda.ru>
2023-03-21 21:39:47 +0300

    CMake: FetchDeps.cmake fixes

--- FetchDeps.cmake
+++ FetchDeps.cmake
@@ -7,21 +7,17 @@ set(Catch2_repository_tag devel)
 
 set(deps panda-lib)
 
-set(NET_SOCKADDR_TESTS ON)
-
-if (${UNIEVENT_TESTS})
+if (${NET_SOCKADDR_TESTS})
     list(APPEND deps Catch2)
 endif()
 
 foreach(dep ${deps})
-    if (NOT ${dep}_POPULATED)
-        if (NOT DEFINED ${${dep}_repository_tag})
-            set (${${dep}_repository_tag} master)
-        endif()
-        FetchContent_Declare(${dep}
-            GIT_REPOSITORY ${${dep}_repository}
-            GIT_TAG ${${dep}_repository_tag}
-        )
-        FetchContent_MakeAvailable(${dep})
-    endif ()
+    if (NOT DEFINED ${${dep}_repository_tag})
+        set (${${dep}_repository_tag} master)
+    endif()
+    FetchContent_Declare(${dep}
+        GIT_REPOSITORY ${${dep}_repository}
+        GIT_TAG ${${dep}_repository_tag}
+    )
 endforeach()
+FetchContent_MakeAvailable(${deps})
